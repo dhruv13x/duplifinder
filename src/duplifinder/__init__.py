d6df5351d4ec57689e1317a6f8ea7a5c9d0bb587
dhruv13x <dhruv13x@gmail.com>
2025-11-02 12:49:16 +0530

    Initial commit

--- src/duplifinder/__init__.py
+++ src/duplifinder/__init__.py
@@ -2,7 +2,7 @@
 
 """Duplifinder: Detect duplicate Python definitions across projects."""
 
-__version__ = "6.0.0"
+__version__ = "6.0.2"
 
 from . import main  # For entrypoint
 from .config import Config
