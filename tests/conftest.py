1b8dd5f86f4e2355bc22b3d4568a460187fd5a64
dhruv13x <dhruv13x@gmail.com>
2025-11-11 01:24:10 +0530

    routine backup

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from unittest.mock import Mock, patch
 
 import pytest
-from src.duplifinder.config import Config
+from duplifinder.config import Config
 from duplifinder.utils import discover_py_files
 
 
